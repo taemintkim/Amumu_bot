57d5e1965a87d1e1f2361ece5954784e2bc56e2b
ThomasKim <TK@TKs-MacBook-Pro.local>
2015-01-09 15:16:39 -0800

    Completed Amumu_bot

--- amumu_bot.py
+++ amumu_bot.py
@@ -4,63 +4,65 @@ import sys
 import datetime
 import random
 
-r = praw.Reddit('Amumu Bot by /u/Xwerve and /u/liquidized')
 username = accounts.amumu_bot_user
 password = accounts.amumu_bot_pass
-subreddit = r.get_subreddit('leagueoflegends')
-comment_subreddits = 'leagueoflegends'
-r.login(username, password)
-posts = subreddit.get_new()
-comments = r.get_comments(comment_subreddits)
-key_phrase = ['ok', 'ok.', 'taunt']
-key_words = ['rammus', 'spiky armadillo']
+subreddits = 'redditdebuggingspace'
+
+r = praw.Reddit('Amumu Bot by /u/Xwerve and /u/liquidized')
+def initialize():
+	r.login(username, password)
+
+initialize()
 comment_ids = []
 post_ids = []
+scanned_ids = []
 
-init_responses = ["Let me give you a hug.", "Let's be friends forever.", "Would you like to be friends?", "Come play with me."]
-yes_response = "I thought you'd never pick me."
-no_response = "Awww. :'("
+##### Triggers and Responses of Amumu_bot in lower case #######
+pre_responses = ["Let me give you a hug.", "Let's be friends forever.", "Would you like to be friends?", "Come play with me."]
+sad_responses = ["I sense that you are sad. "+response for response in pre_responses]
+yes_responses = ["I thought you'd never pick me. (づ｡◕‿‿◕｡)づⒽⓤⒼ♥"]
+no_responses = ["Awww. :(", "Hey, come back! ;___;"]
+yes_triggers = ["ok", "yes", "yeah", "yea", "okay", "yep", "let's be friends", "yee", "i love you", " ya ", " ye ", "sure", "all right", "alright", "i want to be your friend", "hug me", "i love hugs", "<3", "i want a hug", "give me a hug", "come here", "/hug", "*hug"]
+no_triggers = ["no ", "no, " "nope", "naw ", "sorry", "don't want you", "go away", "fuck off", "one wants to play with you"]
+sad_triggers = ["i'm depressed", "i am depressed", "i'm sad", ":'(", "i am lonely", 
+"i'm lonely", "i have no friends", "i want friends", "i'm crying", " :'(", "sad amumu", ";_;", " )':", ";__;", "i need a hug"]
+########################################################################################
 
-def reply_2_comment(comment, trigger):
-	for phrase in key_phrase:
-		if phrase == str(comment).lower() and str(comment.author) != username and str(comment.id) not in comment_ids:
-			print(comment, 'posted by', comment.author)
-			comment.upvote()
-			comment_ids.append(str(comment.id))
-			comment.reply('ok')
-	for word in key_words:
-		if word in str(comment).lower() and str(comment.author) != username and str(comment.id) not in comment_ids:
-			current_time = datetime.datetime.now().time()
-			print(comment, 'posted by', comment.author, 'at', current_time.isoformat())
-			comment.upvote()
-			comment_ids.append(str(comment.id))
-			comment.reply('ok')
-def reply_2_post(post, trigger):
-	for word in key_words:
-		if (word in str(post).lower() or word in str(post.selftext).lower()) and str(post.id) not in post_ids:
-			current_time = datetime.datetime.now().time()
-			try:
-				print(post, 'posted by', post.author, 'at', current_time.isoformat())
-				post.upvote()
-				post_ids.append(str(post.id))
-				post.add_comment('ok')
-			except praw.errors.APIException:
-				print("POST TOO OLD")	
+def reply_2_comments(comment, triggers, responses):
+	commenttext = comment.body.lower()
+	has_trigger = any(trigger in commenttext for trigger in triggers)
+	if has_trigger and str(comment.author) != username and comment.id not in comment_ids:
+		comment_ids.append(comment.id)
+		reply0 = random.choice(responses)
+		comment.reply(reply0)
+		current_time = datetime.datetime.now().time()
+		print('Bot replied to comment', comment, 'posted by', comment.author, 'at', current_time.isoformat())
+		return True
+	else:
+		return False
 
-while True:
-	if len(comment_ids) > 100:
+def has_replied(comment): #check if the scanned comment is a response to an Amumu_bot's comment and respond to it accordingly.
+	parent_id = comment.parent_id
+	parent_comment = r.get_info(None, parent_id, None) 
+	if type(parent_comment) == praw.objects.Comment: #in case parent_comment is not a Comment object.
+		commenttext = parent_comment.body
+		has_trigger = any(trigger in commenttext for trigger in sad_responses)
+		if str(parent_comment.author).lower() == username.lower() and has_trigger:
+			reply_2_comments(comment, yes_triggers, yes_responses) or reply_2_comments(comment, no_triggers, no_responses)
+		else:
+			reply_2_comments(comment, sad_triggers, sad_responses)
+	else:
+		reply_2_comments(comment, sad_triggers, sad_responses)
+
+### main ###
+for raw_comment in praw.helpers.comment_stream(r, subreddits, limit=25, verbosity=0): #go through a stream of new comments in subreddit.
+	if len(comment_ids) > 200:
 		comment_ids.pop(0)
-	try:
-		comment = next(comments)
-		post = next(posts)
-	except StopIteration:
-		posts = subreddit.get_new()
-		comments = r.get_comments('leagueoflegends')
-		comment = next(comments)
-		post = next(posts)
-	except:
- 		os.execv(__file__, sys.argv)
- 	reply_2_comment(comment)
- 	reply_2_post(post)
+	if len(scanned_ids) > 300:
+		scanned_ids.pop(0)
+	if raw_comment.id not in scanned_ids:
+		scanned_ids.append(raw_comment.id)
+		has_replied(raw_comment)
+
 
 
